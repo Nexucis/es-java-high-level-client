fe4e492f27f44c2abecefa3eae2649ac37ed412b
Augustin Husson <husson.augustin@gmail.com>
2018-04-09 22:09:58 +0200

    implement find methods

--- src/main/java/nexucis/elasticsearch/client/namespace/DocumentNamespace.java
+++ src/main/java/nexucis/elasticsearch/client/namespace/DocumentNamespace.java
@@ -1,13 +1,25 @@
 package nexucis.elasticsearch.client.namespace;
 
 import nexucis.elasticsearch.data.annotation.Document;
+import nexucis.elasticsearch.data.type.Page;
 import nexucis.elasticsearch.utils.JsonUtils;
 import nexucis.elasticsearch.utils.StringUtils;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class DocumentNamespace extends AbstractNamespace {
 
@@ -29,6 +41,56 @@ public class DocumentNamespace extends AbstractNamespace {
         T entity = JsonUtils.getObjectFromString(response.getSourceAsString(), clazz);
 
         return entity;
+    }
+
+    public <T> Optional<T> findOne(QueryBuilder queryBuilder, Class<T> clazz) throws IOException {
+        Page<T> page = this.find(queryBuilder, 0, 1, clazz);
+
+        if (page.getHits().size() == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(page.getHits().get(0));
+    }
+
+    public <T> Page<T> findAll(int from, int size, Class<T> clazz) throws IOException {
+        return this.find(QueryBuilders.matchAllQuery(), from, size, clazz);
+    }
+
+    public <T> Page<T> find(QueryBuilder queryBuilder, int from, int size, Class<T> clazz) throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(queryBuilder)
+                .from(from)
+                .size(size);
+        searchRequest.source(searchSourceBuilder);
+
+        return this.find(searchRequest, clazz);
+    }
+
+    public <T> Page<T> find(SearchRequest searchRequest, Class<T> clazz) throws IOException {
+        SearchResponse searchResponse = client.search(searchRequest);
+
+        Page<T> page = new Page<>();
+
+        if (!RestStatus.OK.equals(searchResponse.status())) {
+            return page;
+        }
+
+        SearchHits hits = searchResponse.getHits();
+        long totalElement = hits.getTotalHits();
+        List<T> pageHits = new ArrayList<>();
+
+        for (SearchHit hit : hits.getHits()) {
+            T entity = JsonUtils.getObjectFromString(hit.getSourceAsString(), clazz);
+            pageHits.add(entity);
+        }
+
+        page.setTotalElement(totalElement)
+                .setTotalPage(totalElement/searchRequest.source().size())
+                .setHits(pageHits);
+
+        return page;
     }
 
 }
--- src/main/java/nexucis/elasticsearch/data/type/Page.java
+++ src/main/java/nexucis/elasticsearch/data/type/Page.java
@@ -1,6 +1,7 @@
 package nexucis.elasticsearch.data.type;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Page<T> implements Serializable {
@@ -13,6 +14,12 @@ public class Page<T> implements Serializable {
 
     private List<T> hits;
 
+    public Page() {
+        this.totalElement = 0;
+        this.totalPage = 0;
+        this.hits = new ArrayList<>();
+    }
+
     public Page(long totalElement, long totalPage, List<T> hits) {
         this.totalElement = totalElement;
         this.totalPage = totalPage;
@@ -23,11 +30,26 @@ public class Page<T> implements Serializable {
         return totalElement;
     }
 
+    public Page<T> setTotalElement(long totalElement) {
+        this.totalElement = totalElement;
+        return this;
+    }
+
     public long getTotalPage() {
         return totalPage;
     }
 
+    public Page<T> setTotalPage(long totalPage) {
+        this.totalPage = totalPage;
+        return this;
+    }
+
     public List<T> getHits() {
         return hits;
+    }
+
+    public Page<T> setHits(List<T> hits) {
+        this.hits = hits;
+        return this;
     }
 }
