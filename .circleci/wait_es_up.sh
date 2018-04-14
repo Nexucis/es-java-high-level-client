82f69284d23a69dffb904685b1ed3efdad386e2d
Augustin Husson <husson.augustin@gmail.com>
2018-04-14 22:55:46 +0200

    add script for waiting elasticsearch

---
+++ .circleci/wait_es_up.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+loop=0
+endLoop=4
+wait=1
+
+# for some strange reason, HEAD request go in infinity loop ...
+curl -s -XGET http://localhost:9200 > /dev/null
+response=$?
+
+while [ ${response} != 0 ] && [ ${loop} != ${endLoop} ]; do
+    sleep ${wait}
+    curl -s -XGET http://localhost:9200 > /dev/null
+    response=$?
+    loop=$(( $loop + 1))
+    wait=$(( $wait*2))
+done
+
+if [ ${response} != 0 ]; then
+    echo "elasticsearch is not available"
+    exit 1;
+else
+    echo "elasticsearch is available, starting php test"
+fi
